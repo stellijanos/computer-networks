f49293c938b33a3f56c9cf4396d4510b8fb50c49
Janos Stelli <stellijanos23@gmail.com>
2023-10-10 17:45:40 +0300

    fix problem 1.3 regex and solve problem 1.4

--- probleme/set_1/problema 1.4/server.php
+++ probleme/set_1/problema 1.4/server.php
@@ -6,10 +6,11 @@ socket_bind($socket, '0.0.0.0', 5555);
 
 $length = 1000;
 $flags = 0;
+
 socket_recvfrom($socket, $client_data, $length, $flags, $client_address, $client_port);
 
-echo "Recieved from: $client_address:$client_port => $client_data";
+echo "Recieved from: $client_address:$client_port => \n $client_data";
 
-$server_data = preg_match_all('/[aeiou]/i', $client_data, $matches);
+$server_data = preg_match_all('/[bcdfghjklmnpqrstvwxyz]/i', $client_data, $matches);
 
 socket_sendto($socket, $server_data, $length, $flags, $client_address, $client_port);
