9908532ea3805af58829ad4d51c21f4c7b09f790
Janos Stelli <stellijanos23@gmail.com>
2023-10-10 16:32:15 +0300

    solve first problem

--- probleme/1_set/problema 1.1/client.php
+++ probleme/1_set/problema 1.1/client.php
@@ -4,17 +4,17 @@ $domain = AF_INET;
 $type = SOCK_DGRAM;
 $protocol = 0;
 
-$socket = socket_create($domain, $type, $protocol);
+$socket = socket_create($domain, $type, $protocol); // create socket
 
-$client_data = "Client Data";
-$length = strlen($client_data) ;
+$client_data = "J";
+$length = 100;
 $flags = 0;
 $server_address = "127.0.0.1";
 $server_port = 5555;
 
-socket_sendto($socket, $client_data, $length, $flags, $server_address, $server_port);
-socket_recvfrom($socket, $server_data, $length, $flags, $server_address, $server_port);
+socket_sendto($socket, $client_data, $length, $flags, $server_address, $server_port); // send data to server 
 
-echo "Recieved from: '$server_address:$server_port' the following data: \n $server_data";
+socket_recvfrom($socket, $server_data, $length, $flags, $server_address, $server_port);  // recieve data from server
+echo "Recieved from: '$server_address:$server_port' the following data: \n $server_data"; // print out recieved data from server
 
 socket_close($socket);
--- probleme/1_set/problema 1.1/server.php
+++ probleme/1_set/problema 1.1/server.php
@@ -4,23 +4,23 @@ $domain = AF_INET;
 $type = SOCK_DGRAM;
 $protocol = 0;
 
-$socket = socket_create($domain, $type, $protocol);
+$socket = socket_create($domain, $type, $protocol); // create socket
 
 $address = '0.0.0.0';
 $port = 5555;
 
-socket_bind($socket, $address, $port);
+socket_bind($socket, $address, $port); // bind the associate address and port
 
 $length = 100;
 $flags = 0;
 
-socket_recvfrom($socket, $client_data, $length, $flags, $client_address, $client_port);
+socket_recvfrom($socket, $client_data, $length, $flags, $client_address, $client_port); // recieve sendto data from client
 
-echo "Recieved from: '$client_address: $client_port'\n $client_data";
+echo "Recieved from: '$client_address: $client_port'\n $client_data"; // print out sendto data from client
 
 // additional operations 
 
-$server_data = 'Server Data';
+$server_data = $client_data.$client_data;
 
-socket_sendto($socket, $server_data, $length, $flags, $client_address, $client_port);
+socket_sendto($socket, $server_data, $length, $flags, $client_address, $client_port); // send data (answer) to client 
 
