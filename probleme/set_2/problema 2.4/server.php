faa4f4056b26e5e82eef7b9979fe1bcc195cdb4e
Janos Stelli <stellijanos23@gmail.com>
2023-10-10 21:02:09 +0300

    implement problem 2.4

---
+++ probleme/set_2/problema 2.4/server.php
@@ -0,0 +1,22 @@
+<?php
+
+$socket = socket_create(AF_INET, SOCK_DGRAM, 0);
+
+socket_bind($socket, '0.0.0.0', 5555);
+
+socket_recvfrom($socket, $client_data, 1000, 0, $client_address, $client_port);
+
+echo "Recieved from $client_address:$client_port => $client_data";
+
+$wordsArray = explode(' ',$client_data);
+
+$mostNrOfVowels = 0;
+
+foreach($wordsArray as $word) {
+    $nrVowels = preg_match_all('/[aeiou]/i', $word, $mathes);
+    if ( $nrVowels > $mostNrOfVowels) {
+        $mostNrOfVowels = $nrVowels;
+    }
+}
+
+socket_sendto($socket, $mostNrOfVowels, 1000, 0, $client_address, $client_port);
