ea9befe80b183eb70662b5c684469ae3172163e1
Janos Stelli <stellijanos23@gmail.com>
2023-10-11 08:25:51 +0300

    remove json_decode

--- probleme/set_4/problema 4.4/server.php
+++ probleme/set_4/problema 4.4/server.php
@@ -10,9 +10,6 @@ socket_recvfrom($socket, $client_data, 1000, 0, $client_address, $client_port);
 
 echo "Recieved from $client_address:$client_port => $client_data";
 
-$client_data = json_decode($client_data, true);
-
-
 $numbersAsString = [
     0 => 'zero',
     1 => 'una',
